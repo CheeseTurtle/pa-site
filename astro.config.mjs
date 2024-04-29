dc6314439098c28e3b2b9b3368c991c78600b1b3
CheeseTurtle <4154751+CheeseTurtle@users.noreply.github.com>
2024-04-29 16:46:09 -0500

    Fixed relative URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cheeseturtle.github.io',
-	base: 'pa-site',
+	base: '/pa-site/',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
