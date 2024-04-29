18a19765afba8a4664cc8508844d7a8069723981
CheeseTurtle <4154751+CheeseTurtle@users.noreply.github.com>
2024-04-29 11:55:43 -0500

    Switched back to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pa-site.cheeseturtle.github.io',
+	base: 'pa-site',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
